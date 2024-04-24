1ea2dfc6ab15ffc0961d8f468a921c5088be0666
Eli Vermeulen <eli.vermeulen2002@gmail.com>
2024-04-24 17:37:43 +0200

    Add agent obstacles and code

--- Assets/Scripts/CubeAgentRays.cs
+++ Assets/Scripts/CubeAgentRays.cs
@@ -7,40 +7,59 @@ using Unity.MLAgents.Actuators;
 
 public class CubeAgentRays : Agent
 {
-    // private bool jump = true;
-    // public Rigidbody rb;
-    // public override void OnEpisodeBegin(){
-    //     jump = true;
+    private bool jump = true;
+    public Rigidbody rb;
+    public float jumpForce;
+    public override void OnEpisodeBegin()
+    {
+        jump = true;
 
-    //     this.transform.localPosition = new Vector3(0.17f,0.57f,3.79f);
-    //     rb.transform.rotation = new Quaternion.Euler(0,-180,0);
-    // }
+        this.transform.localPosition = new Vector3(0.17f, 0.57f, 3.79f);
+        rb.transform.rotation = Quaternion.Euler(0, -180, 0);
+    }
 
-    // public override void CollectObservations(VectorSensor sensor){
-    //     sensor.AddObservation(this.transform.localPosition);
-    // }
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        sensor.AddObservation(this.transform.localPosition);
+    }
 
-    // public override void OnActionReceived(ActionBuffers actionBuffers){
-    //     if (actionBuffers.ContinuousAction[0] > 0 && jump){
-    //         rb.AddForce(Vector3.up * 340, ForceMode.Acceleration);
-    //         jump = false;
-    //     }
-    // }
+    public override void OnActionReceived(ActionBuffers actionBuffers)
+    {
+        if (actionBuffers.ContinuousActions[0] > 0 && jump)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Acceleration);
+            jump = false;
+        }
+    }
 
-    // public override void Heuristic(in ActionBuffers actionsOut){
-    //     var continuousActionsOut = actionsOut.ContinuousActions;
-    //     continuousActionsOut[0]=Input.GetAxis("Vertical");
-    // }
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        var continuousActionsOut = actionsOut.ContinuousActions;
+        continuousActionsOut[0] = Input.GetAxis("Vertical");
+    }
 
-    // private void OnCollisionStay(Collision collision){
-    //     if (collision.gameObject.CompareTag("hit")){
-    //         AddReward(-1f);
-    //         Destroy(collision.gameObject);
-    //         EndEpisode();
-    //     }
-    //     else if (collision.gameObject.CompareTag("point")){
-    //         AddReward(1f);
-    //         Destroy(collision.gameObject);
-    //     }
-    // }
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("hit"))
+        {
+            AddReward(-1f);
+            Debug.Log('h');
+            Destroy(collision.gameObject);
+            EndEpisode();
+        }
+        else if (collision.gameObject.CompareTag("point"))
+        {
+            AddReward(1f);
+            Destroy(collision.gameObject);
+        }
+        if (collision.gameObject.CompareTag("ground"))
+        {
+            jump = true;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+    }
 }
---
+++ Assets/Scripts/DeleteObstacles.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeleteObstacles : MonoBehaviour
+{
+    [SerializeField] private GameObject prefabToSpawn;
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("hit"))
+        {
+            Vector3 spawnpoint = new Vector3 (0.19f, 0.71f, -4.85f);
+            Instantiate(prefabToSpawn, spawnpoint, Quaternion.identity);
+        }
+        Destroy(collision.gameObject);
+
+    }
+
+}
